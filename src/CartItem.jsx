fa7caa79aaa24a594c0c96913d462ae6159c390b
Mtrmn <mrymtrmn@gmail.com>
2024-12-23 14:09:36 -0500

    dsfds

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,41 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    items.forEach((item) => {
+    totalCost += item.quantity * item.cost;
+    });
   };
 
   const handleContinueShopping = (e) => {
-   
+   props.onContinueShopping();
   };
 
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
-
-  const handleIncrement = (item) => {
-  };
+const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+};
 
   const handleDecrement = (item) => {
-   
+   if (item && item.quantity > 1){
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+    dispatch(removeItem(item));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-  };
+    let totalspecialCost = 0;
+    totalspecialCost += item.quantity * item.cost;
+    }
 
   return (
     <div className="cart-container">
